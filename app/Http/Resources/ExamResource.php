b23add084237046745f931642050274e9b22a414
Your Name <amirtaja@yahoo.com>
2022-11-03 13:19:25 +0330

    f

--- app/Http/Resources/ExamResource.php
+++ app/Http/Resources/ExamResource.php
@@ -28,7 +28,7 @@ class ExamResource extends JsonResource
             "image" => $this->image,
             "is_active" => $this->is_active,
             "file" => $this->file?->url,
-            "file_raw" => env("APP_URL") . "/exams/" . md5($this->id) . ".pdf",
+            "file_raw" => env("APP_URL") . "/exams/" . md5($this->id .  "drsho1") . ".pdf",
             "completed" =>  $session?->completed ? 1 : 0,
             "session" => ExamSessionResource::make($session),
             "resumable" => ! $session?->completed  &&  $session

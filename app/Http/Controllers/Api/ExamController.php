98e60e9f03f18ff152419663cd9a7b78a5ee7e15
Your Name <amirtaja@yahoo.com>
2022-11-03 17:15:20 +0330

    f

--- app/Http/Controllers/Api/ExamController.php
+++ app/Http/Controllers/Api/ExamController.php
@@ -109,10 +109,7 @@ class ExamController extends Controller
         header('Access-Control-Allow-Origin', '*');
         header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
         return Response::make(file_get_contents($file), 200, 
-        [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="' . $hash . '"'
-        ]
+       
     
     );
     }
